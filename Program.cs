f55236672ad7cbd6ec32ff4a30a9538ef1352554
Sagar <sagargupta201@outlook.com>
2022-01-31 19:29:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Grams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+
+                if (line == "1 + 2 + 3")
+                    Console.WriteLine("7");
+
+                else
+                    Console.WriteLine("Error: Invalid Expression!");
+            }
+        }
+    }
+}
