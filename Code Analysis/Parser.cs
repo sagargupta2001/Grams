9d47be87470ddd8768c7e67c1eeb0a4b4bcaf2d6
Sagar <sagargupta201@outlook.com>
2022-02-02 13:17:18 +0530

    Move precedences to SyntaxFacts

--- Code Analysis/Parser.cs
+++ Code Analysis/Parser.cs
@@ -70,7 +70,7 @@
             
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
 
@@ -82,22 +82,7 @@
             return left;
         }
 
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;
-            }
-        }        
+         
 
         private ExpressionSyntax ParsePrimaryExpression()
         {
