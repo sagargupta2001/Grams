c653aee8fa12536c17a4a9e7ca1341a0c7708868
Sagar <sagargupta201@outlook.com>
2022-02-02 13:40:53 +0530

    Added support for unary operators

--- Code Analysis/Evaluator.cs
+++ Code Analysis/Evaluator.cs
@@ -24,6 +24,19 @@ namespace Grams.Code_Analysis
             if (node is LiteralExpressionSyntax n)
                 return (int)n.literalToken.Value;
 
+            if (node is UnaryExpressionSyntax u)
+            {
+                var operand = EvaluateExpression(u.Operand);
+
+                if (u.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return operand;
+                else if (u.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return -operand;
+                else 
+                    throw new Exception($"Unexpected Unary Operator {u.OperatorToken.Kind}");
+
+            }
+
             if (node is BinaryExpressionSyntax b)
             {
                 var left = EvaluateExpression(b.Left);
